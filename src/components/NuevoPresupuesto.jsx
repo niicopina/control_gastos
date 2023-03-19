57ee661dff0d217dc57e51617fbf89237b60a031
nicopina <pinanicolasagustin@gmail.com>
2023-03-19 13:53:25 -0300

    mensaje

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import {useState} from 'react'
+import Mensaje from './Mensaje'
 
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState('')
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault()
+
+        if(!Number(presupuesto) || Number(presupuesto) < 0 ){
+            setMensaje('no es un ppto valido')
+        }else{
+            console.log('si es un numero')
+        }
+    }
+
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
+        <form className='formulario' onSubmit={handlePresupuesto}>
             <div className='campo'>
                 <label htmlFor="">Definir Presupuesto</label>
                 <input type="text" className='nuevo-presupuesto'
@@ -14,6 +29,7 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
                 />
             </div>
             <input type="submit" value="Añadir" />
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
         </form>
     </div>
   )
