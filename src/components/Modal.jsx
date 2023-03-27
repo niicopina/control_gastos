c05d750bed3aece45f3315c37c2a395925c4334f
nicopina <pinanicolasagustin@gmail.com>
2023-03-27 18:23:22 -0300

    añadir gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -19,6 +19,38 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
         </div>
         <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
             <legend>Nuevo Gasto</legend>
+            <div className="campo">
+              <label htmlFor="nombre">Nombre Gasto</label>
+              <input 
+                  id='nombre'
+                  type="text"
+                  placeholder='Añade el nombre del gasto' />
+            </div>
+            <div className="campo">
+              <label htmlFor="cantidad">Cantidad</label>
+              <input 
+                  id='cantidad'
+                  type="number"
+                  placeholder='Añade la cantidad del gasto: ej 40000' />
+            </div>
+            <div className="campo">
+              <label htmlFor="categoria">Categoría</label>
+              <select name="" id="categoria">
+                <option value="">Selecciona</option>
+                <option value="ahorro">Ahorro</option>
+                <option value="comida">Comida</option>
+                <option value="casa">Gastos varios</option>
+                <option value="ocio">Ocio</option>
+                <option value="salud">Salud</option>
+                <option value="suscripciones">Suscripciones</option>
+              </select>
+            </div>
+
+            <input 
+                type="submit"
+                value='Añadir Gasto'
+
+              />
         </form>
     </div>
   )
