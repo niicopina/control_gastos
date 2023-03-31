5a717c2c78557c5fc532c146ae3a3defcee0cd5e
nicopina <pinanicolasagustin@gmail.com>
2023-03-31 20:54:44 -0300

    date now

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { formatearFecha } from '../helpers'
 
-const Gasto = () => {
+const Gasto = ({gasto}) => {
+    const { categoria, nombre, cantidad, id, fecha } = gasto
+
   return (
-    <div>Gasto</div>
+    <div className='gasto sombra'>
+        <div className='contenido-gasto'>
+            
+            <div className="descripcion-gasto">
+                <p className='categoria'>{categoria}</p>
+                <p className='nombre-gasto'>{nombre}</p>
+                <p className='fecha-gasto'>
+                    Agregado el: {''} 
+                    <span>{formatearFecha(fecha)}</span>
+                </p>
+            </div>
+        </div>
+        <p className='cantidad-gasto'>${cantidad}</p>
+    </div>
   )
 }
 
--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -4,8 +4,8 @@ import Gasto from './Gasto'
 const ListadoGastos = ({gastos}) => {
   return (
     <div className='listado-gastos contenedor'>
-        <h2>{ListadoGastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
-        {ListadoGastos.map(gasto => (
+        <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
+        {gastos.map(gasto => (
             <Gasto 
                 key={gasto.id}
                 gasto={gasto} />
